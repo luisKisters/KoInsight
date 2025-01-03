59db48be459923cac5fa1e49c70841e1786b58e7
Georgi Gardev <georgi@gar.dev>
2025-01-03 13:52:18 +0200

    Update VITE_API_URL

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = `${import.meta.env.VITE_API_URL}/api`;
+export const API_URL = `${import.meta.env.VITE_API_URL ?? ''}/api`;
 
 export async function fetchFromAPI<T>(endpoint: string, method: string = 'get') {
   const response = await fetch(`${API_URL}/${endpoint}`, {
