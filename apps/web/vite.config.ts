488dce88062fed8ffbc479e8d2354a4e673b255d
Georgi Gardev <georgi@gar.dev>
2025-04-06 11:28:39 +0300

    Fix env vars

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const PORT = Number(process.env.PORT ?? 3000);
-const HOST = process.env.HOST || 'localhost';
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const PORT = Number(process.env.VITE_WEB_PORT ?? 3000);
+  const HOST = process.env.VITE_WEB_HOST || 'localhost';
 
-export default defineConfig({
-  plugins: [react()],
-  css: { postcss: './postcss.config.cjs' },
-  server: { host: HOST, port: PORT },
-  build: {
-    target: 'esnext',
-    outDir: './dist',
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+  return defineConfig({
+    plugins: [react()],
+    css: { postcss: './postcss.config.cjs' },
+    server: { host: HOST, port: PORT },
+    build: {
+      target: 'esnext',
+      outDir: './dist',
+      emptyOutDir: true,
     },
-  },
-});
+    resolve: {
+      alias: {
+        // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+        '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+      },
+    },
+  });
+};
