3f2dedfe8b6e28404e1d82e5f4bb9a62381b9616
Georgi Gardev <georgi@gar.dev>
2025-04-07 18:31:06 +0300

    use order

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -21,12 +21,10 @@ const ENV = process.env.NODE_ENV;
 
 async function setupServer() {
   const app = express();
-  app.use(express.json());
-  app.use(morgan('tiny'));
-
   // Increase the limit to be able to upload the whole database
   app.use(express.json({ limit: '1000mb' }));
   app.use(express.urlencoded({ limit: '1000mb', extended: true }));
+  app.use(morgan('tiny'));
 
   if (ENV === 'development') {
     // Allow requests from dev build
