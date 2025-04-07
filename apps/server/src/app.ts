d1f4971240c219bb3123cefbbc409023d9819c2a
Georgi Gardev <georgi@gar.dev>
2025-04-07 18:25:35 +0300

    Increase limit

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -24,6 +24,10 @@ async function setupServer() {
   app.use(express.json());
   app.use(morgan('tiny'));
 
+  // Increase the limit to be able to upload the whole database
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb' }));
+
   if (ENV === 'development') {
     // Allow requests from dev build
     app.use(cors({ origin: '*' }));
