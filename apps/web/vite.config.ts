f63358c77cd3a3db6ee491dd6f3c1a0f69b0917c
GeorgeSG <georgi@gar.dev>
2025-04-11 12:29:29 +0300

    fix env loading

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
   const PORT = Number(process.env.VITE_WEB_PORT ?? 3000);
   const HOST = process.env.VITE_WEB_HOSTNAME || 'localhost';
 
